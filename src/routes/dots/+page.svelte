6209621d7485b09bb72366df30b7c95fd297b406
FirebirdFrankie <firebirdfrankie13@gmail.com>
2024-03-24 23:45:57 +0000

    Trying out adding some pages.

---
+++ src/routes/dots/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Ratings } from '@skeletonlabs/skeleton';
+    import type { PageData } from './$types';
+    let value = { current: 3.5, max: 5 };
+    export let data: PageData;
+</script>
+
+<Ratings bind:value={value.current} max={value.max}>
+	<svelte:fragment slot="empty">(icon)</svelte:fragment>
+	<svelte:fragment slot="half">(icon)</svelte:fragment>
+	<svelte:fragment slot="full">(icon)</svelte:fragment>
+</Ratings>
