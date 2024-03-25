b68d80fbd92f384cdf0a7a1bd4ea3a7a69f200c2
FirebirdFrankie <firebirdfrankie13@gmail.com>
2024-03-25 00:54:19 +0000

    Added Iconify with UnPlugin. Messed around with Ratings Component and assigned them to hours in an array.

--- src/routes/dots/+page.svelte
+++ src/routes/dots/+page.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
 	import { Ratings } from '@skeletonlabs/skeleton';
+	import Fa6SolidCircle from '~icons/fa6-solid/circle';
+	import Fa6RegularCircle from '~icons/fa6-regular/circle';
     import type { PageData } from './$types';
-    let value = { current: 3.5, max: 5 };
+	
     export let data: PageData;
+
+    let value = { current: 2, max: 4 };
+	
+
+	//const hours = new Array(24).fill(null).map((_, i) => i + 1);
+	const hours = new Array(24).fill(0);
+
+	// function iconClick(event: CustomEvent<{index:number}>, hourState:number): void {
+	// 	hourState = event.detail.index;
+	// }
+	
 </script>
 
-<Ratings bind:value={value.current} max={value.max}>
-	<svelte:fragment slot="empty">(icon)</svelte:fragment>
-	<svelte:fragment slot="half">(icon)</svelte:fragment>
-	<svelte:fragment slot="full">(icon)</svelte:fragment>
-</Ratings>
+<h1>
+	Today
+</h1>
+
+{#each hours as hourState}
+	<Ratings bind:value={hourState} max={value.max} interactive on:icon={(e)=>(hourState = e.detail.index)}>
+		<svelte:fragment slot="empty"><Fa6RegularCircle /></svelte:fragment>
+		<svelte:fragment slot="full"><Fa6SolidCircle /></svelte:fragment>
+	</Ratings>
+{/each}
+
